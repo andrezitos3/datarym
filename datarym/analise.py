a8fecf955b96fb232d91930aeebbaa8857def687
Andre <65464498+andrezitos3@users.noreply.github.com>
2024-06-14 19:14:43 -0300

    input feito e recomendador em desenvolvimento

--- datarym/analise.py
+++ datarym/analise.py
@@ -88,12 +88,29 @@ def organizaCsv():
     df = pd.DataFrame(processed_data, columns=columns)
 
     # Exibir o DataFrame
-    #print(df)
+    # print(df)
 
     return df
 
+# Função para formatar a string
+def formatar_strings_lista_input(s):
+    # Inicialmente, definimos o primeiro caractere como maiúsculo
+    s = s.capitalize()
+    # Iteramos sobre os caracteres da string
+    resultado = []
+    for i in range(len(s)):
+        # Se o caractere atual for um espaço ou hífen, o próximo caractere deve ser maiúsculo
+        if s[i] in [' ', '-']:
+            resultado.append(s[i])
+            if i + 1 < len(s):
+                resultado.append(s[i + 1].upper())
+        # Caso contrário, apenas adicionamos o caractere atual
+        elif i == 0 or s[i-1] not in [' ', '-']:
+            resultado.append(s[i])
+    # Retornamos a string formatada
+    return ''.join(resultado)
 
-def inputUsuario(df): # botar df de parametro
+def inputUsuario(): # botar df de parametro
 
     print("-" * 30 + " RECOMENDADOR MUSICAL " + "-" *  30)
 
@@ -104,24 +121,40 @@ def inputUsuario(df): # botar df de parametro
     # Remover espaços no início de cada string
     listaFormat = [s.lstrip() for s in listaGen]
 
-    generos = df[['primary_genres', 'secondary_genres']]
+    # Aplicar a formatação em todas as strings da lista
+    lista_final = [formatar_strings_lista_input(s) for s in listaFormat]
 
-    #verifica se tem o input escolha dentro do DataFrame generos para ver se o genero que o usuario existe 
-    for primary,secondary in generos.items():
-        print(f'colunas:{primary}')
-        print(f'generos:\n{secondary}', sep='\n')
-
-    print(listaFormat)
+    return lista_final
 
     #print(generos)
 
+def recomenda(df, escolhas):
+
+    generos = df[['release_name','artist_name', 'primary_genres']]
+
+    #verifica se tem o input escolha dentro do DataFrame generos para ver se o genero que o usuario existe 
+    #for coluna, generos in generos.items():
+    #    print(f'colunas: {coluna}')
+    #    print(f'generos: {generos}', sep='\n')
+    
+    #pd.set_option('display.max_rows', None)
+
+    # Filtrando o DataFrame
+    mask = generos['primary_genres'].str.contains('|'.join(escolhas), case=False, na=False)
+    generos_filtrado = df[mask]
+
+    print(generos_filtrado[['release_name', 'artist_name', 'primary_genres']])
+
+    
+    
 
 def main():
 
     # gerando o dataframe e organizando o csv
-    musicas = organizaCsv()
-    inputUsuario(musicas)
-
+    albuns = organizaCsv()
+    escolhas = inputUsuario()
+    print(escolhas)
+    recomenda(albuns, escolhas)
 
 if __name__ == "__main__":
     main()
